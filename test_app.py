c039e2708b3de23b65129ae4cee0adefd5fae535
jrwblackwell1998 <60676308+jrwblackwell1998@users.noreply.github.com>
2023-08-13 15:16:42 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,3 +1,11 @@
 import streamlit as st 
 
-st.write('This is a test!')
+# Initialize connection.
+conn = st.experimental_connection('snowflake', type='sql')
+
+# Perform query.
+df = conn.query('SELECT * from "SAS_PROD_SELF_SERVE"."PRD_SELFSERVE"."SF_HO_DEEPDIVE_20231_JWB" limit 100')
+
+st.write('This is the size of the dataset: ')
+st.write(df.shape)
+
