9b645c68cc28005e9a82301c783a7117d15afd40
jrwblackwell1998 <60676308+jrwblackwell1998@users.noreply.github.com>
2023-08-13 15:18:13 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -6,6 +6,6 @@ conn = st.experimental_connection('snowflake', type='sql')
 # Perform query.
 df = conn.query('SELECT * from "SAS_PROD_SELF_SERVE"."PRD_SELFSERVE"."SF_HO_DEEPDIVE_20231_JWB" limit 100')
 
-st.write('This is the size of the dataset: ')
+st.write('This is the size of the dataset: ') 
 st.write(df.shape)
 
